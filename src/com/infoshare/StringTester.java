8c4b1be006fe7db573a9cf8012e7f426b31801a9
Karol Kałuża <karol.kaluza@gmail.com>
2020-11-29 01:28:42 +0100

    StringUtils basic methods finish

--- src/com/infoshare/StringTester.java
+++ src/com/infoshare/StringTester.java
@@ -1,34 +1,37 @@
 package com.infoshare;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
 public class StringTester {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        String fullText = "Manifesto for Agile Software Development\n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "We are uncovering better ways of developing\n" +
-                "software by doing it and helping others do it.\n" +
-                "Through this work we have come to value.\n" +
-                "\n" +
-                "Individuals and interactions over processes and tools,\n" +
-                "Working software over, comprehensive documentation,\n" +
-                "Customer collaboration over contract negotiation,\n" +
-                "Responding to change over following a plan.\n" +
-                "\n" +
-                "That is, while there is value in the items on\n" +
-                "the right, we value the items on the left more.";
-
+        String fileName = "testFile.txt";
+        String fullText = Files.readString(Path.of(fileName));
         StringUtils stringUtils = new StringUtils();
 
-        String[] lines = stringUtils.getLines(fullText);                    // zad.1 getLines
-        System.out.println(lines[0]);
+        String[] lines = stringUtils.getLines(fullText);                                                                // ex. getLines
+        System.out.println("Get lines[0]: " + lines[0]);
 
-        String[] words = stringUtils.getWords(lines[5]);                    // zad.2 getWords
-        System.out.println(words[8]);
+        String[] words = stringUtils.getWords(fullText);                                                                // ex. getWords
+        System.out.println("Get words[5]: " + words[5]);
 
-        String [] wordsWithoutSpecialSings = stringUtils.replaceSign(words);  //zad. 2 a) Remove dot and semicolon
-        System.out.println(wordsWithoutSpecialSings[8]);
+        Map <String, Integer> frequencyOfWords = stringUtils.getWordFrequency(fullText);
+        String wordToCheck = "development";
+        System.out.println("Number of appearance word \"" + wordToCheck + "\": " + frequencyOfWords.get(wordToCheck));  // ex. map of words frequency
 
+        String word = words[4];
+        System.out.println("Word without first and last letter: " + stringUtils.removeLetterFirstAndLast(word));        // ex. remove first & last letter
+
+        System.out.println("Number of appearance letter \"e\" in word \"development\": "
+                + stringUtils.countLetters(word, 'e'));                                                           // ex. letter counter
+
+        System.out.println("Reverse word \"development\": " + stringUtils.reverseWord(word));                           // ex. word reverser
+
+        System.out.println("Concatenation: " + stringUtils.concatenate(word, words[0]));                                // ex. concatenation
+
+        System.out.print("Every third symbol from \"development\": ");stringUtils.printEveryThirdSymbol(word);          // ex. print every third symbol
     }
 }
--- src/com/infoshare/StringUtils.java
+++ src/com/infoshare/StringUtils.java
@@ -1,29 +1,89 @@
 package com.infoshare;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class StringUtils {
 
-
-    public String[] getLines(String veryLongText) {
-        String[] lines = veryLongText.split("\n");
+    public String[] getLines(String fullText) {
+        String[] lines = fullText.split("\n");
         return lines;
     }
 
-    public String[] getWords(String oneLineText) {
-        String[] words = oneLineText.split(" ");
+    public String[] getWords(String fullText) {
+        String[] words = fullText.toLowerCase()
+                .replaceAll("[.,]", "")
+                .replaceAll("\n+", " ")
+                .split(" ");
         return words;
     }
-    public String[] replaceSign (String[] wordsWithSign){
-        String [] wordsWithout = wordsWithSign;
-        for (int i = 0; i < wordsWithSign.length; i++) {
-            if (wordsWithSign[i].contains(".")){
-                wordsWithout[i] = wordsWithSign[i].substring(0,wordsWithSign[i].length()-1);
-            }else if (wordsWithSign[i].contains(";")){
-                wordsWithout[i] = wordsWithSign[i].substring(0,wordsWithSign[i].length()-1);
-            }else {
-                wordsWithout[i] = wordsWithSign[i];
+
+    public Map<String, Integer> getWordFrequency(String fullText) {
+        Map<String, Integer> frequencyOfWords = new HashMap<>();
+        String[] words = getWords(fullText);
+        for (String word : words) {
+            if (frequencyOfWords.containsKey(word)) {
+                frequencyOfWords.put(word, frequencyOfWords.get(word) + 1);
+            } else {
+                frequencyOfWords.put(word, 1);
             }
         }
-        return wordsWithout;
+        return frequencyOfWords;
     }
 
+    public String removeLetterFirstAndLast(String word) {
+        return word.substring(1, word.length() - 1);
+    }
+
+    public int countLetters(String word, char letter) {
+        int counter = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == letter) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public String reverseWord(String word) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = word.length() - 1; i >= 0; i--) {
+            builder.append(word.charAt(i));
+        }
+        return builder.toString();
+    }
+
+    public String concatenate(String firstWord, String secondWord) {
+        return firstWord + " " + secondWord;
+    }
+
+    public void printEveryThirdSymbol(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            if ((i + 1) % 3 == 0) {
+                System.out.print(word.charAt(i));
+            }
+        }
+    }
+
+//    public ArrayList<Character> getMostFrequencyChar (String word){           // TODO
+//        Map<Character, Integer> mapCharsFrequency = new HashMap<>();
+//        List<Character> mostFreqChars = new ArrayList<>();
+//        char mostFreqChar = word.charAt(0);
+//        int mostFreqValue = 1;
+//        for (int i = 0; i < word.length(); i++) {
+//            if (mapCharsFrequency.containsKey(word.charAt(i))) {
+//                mapCharsFrequency.put(word.charAt(i), +1);
+//            } else {
+//                mapCharsFrequency.put(word.charAt(i), 1);
+//            }
+//            if (mapCharsFrequency.get(word.charAt(i)) >= mostFreqValue) {
+//                mostFreqChars.add(word.charAt(i));
+//            } else {
+//                mostFreqChars.add(mostFreqChar);
+//            }
+//        }
+//        return (ArrayList<Character>) mostFreqChars;
+//    }
 }
